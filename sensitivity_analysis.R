9be34bdc1929a56c0e94276b9c11a6fac1d17dc4
stefftaelman <stefftaelman@gmail.com>
2023-08-29 11:07:16 +0200

    feat: updated to binary HS and test against shannon div

--- sensitivity_analysis.R
+++ sensitivity_analysis.R
@@ -1,29 +1,92 @@
+library(dplyr)
+
+# set paths
 metadata_path <- "./data/metadata.csv"
+raw_counts_path <- "./data/asv_counts.csv"
+taxonomy_path <- "./data/asv_taxonomy.csv"
 
+# read in data
 metadata <- read.csv(metadata_path, row.names = 1)
-metadata <- metadata[complete.cases(metadata), ]
-head(metadata)
+raw_counts <- data.frame(t(data.table::fread(raw_counts_path)[, -1]))
+seqs <- row.names(raw_counts)
+row.names(raw_counts) <- paste0("ASV", seq(dim(raw_counts)[1]))
+colnames(raw_counts) <- row.names(metadata)
+taxonomy <- as.matrix(read.csv(taxonomy_path, row.names = 1))[seqs, ]
+row.names(taxonomy) <- row.names(raw_counts)
 
+internal_controls <- row.names(metadata[metadata$sample_type == "control", ])
+print(sum(raw_counts[, internal_controls]))
+raw_counts <- raw_counts[, !names(raw_counts) %in% internal_controls]
+metadata <- metadata[!rownames(metadata) %in% internal_controls, !names(metadata) %in% c("fastq_1", "fastq_2", "sample_type")]
+metadata$stable <- as.factor(metadata$stable)
+dim(metadata)
+dim(raw_counts)
 
-### binary (qualitative) regressions
-hs_fgz_bin <- ifelse(metadata$HS_FGZ_QPCR > 0, 1, 0)
-hs_pgz_bin <- ifelse(metadata$HS_PGZ_QPCR > 0, 1, 0)
-fg_po_bin <- ifelse(metadata$FG_PO_QPCR > 0, 1, 0)
-pellet_feed <- ifelse(metadata$group == "Korrel", 1, 0)
+## calculate alpha diversity
+ps_object <- phyloseq::phyloseq(
+  otu_table=phyloseq::otu_table(raw_counts, taxa_are_rows=TRUE),
+  sample_data=phyloseq::sample_data(metadata),
+  tax_table=phyloseq::tax_table(taxonomy)
+)
+alpha_diversity <- phyloseq::estimate_richness(ps_object, split=TRUE, "Shannon")
+alpha_diversity <- merge(x=alpha_diversity, y=metadata, by="row.names")
+alpha_diversity <- alpha_diversity %>%
+    dplyr::mutate(
+        pylorus_hs=ifelse(HS_PGZ_QPCR > 0, 1, 0),
+        fundus_hs=ifelse(HS_FGZ_QPCR > 0, 1, 0),
+        pellet_feed=ifelse(group == "Korrel", 1, 0),
+        stable11=ifelse(stable == 11, 1, 0),
+        stable12=ifelse(stable == 12, 1, 0),
+        stable13=ifelse(stable == 13, 1, 0),
+        stable14=ifelse(stable == 14, 1, 0),
+        stable21=ifelse(stable == 21, 1, 0),
+        stable23=ifelse(stable == 23, 1, 0),
+        ulcer_binary=ifelse(ulcer_score > 2, 1, 0)
+        )
+alpha_diversity$stable <- as.factor(alpha_diversity$stable)
+row.names(alpha_diversity) <- alpha_diversity$Row.names
+alpha_diversity$Row.names <- NULL
+head(alpha_diversity)
 
-# association between hs and fg
-m_hs_fg <- glm(FG_PO_QPCR ~ HS_FGZ_QPCR + stable, data=metadata ,family="gaussian")
-summary(m_hs_fg)
+# association between hs in the fundus and shannon diversity
+m_fhs_sha <- glm(
+  Shannon ~ fundus_hs + stable,
+  data=alpha_diversity,
+  family="gaussian"
+  )
+summary(m_fhs_sha)
 
 sensitivity <- sensemakr::sensemakr(
-    model = m_hs_fg,
-    treatment = "HS_FGZ_QPCR",
-    benchmark_covariates = "stable",
+    model = m_fhs_sha,
+    treatment = "fundus_hs",
+    benchmark_covariates = "stable21",
     kd = c(5, 10)
 )
 
 png(
-  filename = "./figures/HS_FG_sensitivity_analysis.png",
+  filename = "./figures/fundusHS_shannondiv_sensitivity_analysis.png",
+  width=800, height=800, res=150
+  )
+plot(sensitivity)
+dev.off()
+
+# association between hs in the pylorus and shannon diversity
+m_phs_sha <- glm(
+  Shannon ~ pylorus_hs + stable,
+  data=alpha_diversity,
+  family="gaussian"
+  )
+summary(m_phs_sha)
+
+sensitivity <- sensemakr::sensemakr(
+    model = m_phs_sha,
+    treatment = "pylorus_hs",
+    benchmark_covariates = "stable21",
+    kd = c(5, 10)
+)
+
+png(
+  filename = "./figures/pylorusHS_shannondiv_sensitivity_analysis.png",
   width=800, height=800, res=150
   )
 plot(sensitivity)
